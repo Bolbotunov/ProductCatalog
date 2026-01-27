5176d29336df7b134824486ac965d2cad9b5b59e
Bolbotunov Konstantin <100226065+Bolbotunov@users.noreply.github.com>
2026-01-27 15:00:43 +0300

    feat: add base layout for product card

--- src/pages/CatalogPage/index.tsx
+++ src/pages/CatalogPage/index.tsx
@@ -1,5 +1,11 @@
+import ProductCard from '@/components/ProductCard';
+
 function CatalogPage() {
-  return <>CatalogPage</>;
+  return (
+    <>
+      <ProductCard />
+    </>
+  );
 }
 
 export default CatalogPage;
